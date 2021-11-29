48d1f6c21ad7c66ce43a0354656dff46601cd27b
Miguel9000 <miguelangeljm1@hotmail.com>
2021-11-29 11:36:11 -0600

    Prueba 1

---
+++ src/proyectogithubjimenez/ProyectoGitHubJimenez.java
@@ -0,0 +1,16 @@
+
+package proyectogithubjimenez;
+
+
+public class ProyectoGitHubJimenez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       for (int i=0; i < 10000; i++){
+           System.out.println("Prueba esfuerzo GITHUB:" + i);
+       }
+    }
+    
+}
